e0aa706e8787ba1004545da02d82946934e34b40
alifeee <alifeeerenn@hotmail.com>
2023-04-29 16:09:53 +0100

    Read spreadsheet with gspread add credentials load spreadsheet

--- src/spreadsheet.py
+++ src/spreadsheet.py
@@ -1,3 +1,18 @@
+"""
+This file is used to connect to the Google Sheets API.
+"""
 import gspread
+import pandas
 
-print("Hello World!")
+gc = gspread.service_account(filename='google_credentials.json')
+
+sh = gc.open_by_key("18OQs6uJgoyx3zrRhb9tcnBHxpUo4OyyFJKptJHMr-D0")
+
+row_A = sh.sheet1.col_values(1)
+row_B = sh.sheet1.col_values(2)
+
+df = pandas.DataFrame([row_A, row_B]).transpose()
+
+df.columns = ['A', 'B']
+
+print(df)
