5eb4b508e105ae1424b0b69e70bd753821ce32cd
alifeee <alifeeerenn@hotmail.com>
2023-04-29 16:22:41 +0100

    refactor code into class

--- src/spreadsheet.py
+++ src/spreadsheet.py
@@ -4,15 +4,39 @@ This file is used to connect to the Google Sheets API.
 import gspread
 import pandas
 
-gc = gspread.service_account(filename='google_credentials.json')
 
-sh = gc.open_by_key("18OQs6uJgoyx3zrRhb9tcnBHxpUo4OyyFJKptJHMr-D0")
+class Spreadsheet:
+    """
+    A class to connect to the Google Sheets API and view/edit spreadsheets.
+    """
 
-row_A = sh.sheet1.col_values(1)
-row_B = sh.sheet1.col_values(2)
+    def __init__(self, credentials_path, spreadsheet_id):
+        self.gspread_credentials = gspread.service_account(
+            filename=credentials_path)
+        self.spreadsheet = self.gspread_credentials.open_by_key(spreadsheet_id)
 
-df = pandas.DataFrame([row_A, row_B]).transpose()
+    def get_cols_as_df(self, column_numbers: list):
+        """Gets the specified columns as a dataframe.
 
-df.columns = ['A', 'B']
+        Args:
+            column_numbers (list of ints): The column numbers to get. Starts at 1.
 
-print(df)
+        Returns:
+            pandas.DataFrame: The columns as a dataframe.
+        """
+        sheet = self.spreadsheet.sheet1
+        columns = [sheet.col_values(col) for col in column_numbers]
+
+        dataframe = pandas.DataFrame(columns).transpose()
+
+        return dataframe
+
+
+if __name__ == '__main__':
+    CREDENTIALS_PATH = "google_credentials.json"
+    SPREADSHEET_ID = "18OQs6uJgoyx3zrRhb9tcnBHxpUo4OyyFJKptJHMr-D0"
+
+    ss = Spreadsheet(CREDENTIALS_PATH, SPREADSHEET_ID)
+
+    df = ss.get_cols_as_df([1, 2])
+    print(df)
