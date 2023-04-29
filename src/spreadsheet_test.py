838fee5e807c20a941aa2151f5a34244a0602b65
alifeee <alifeeerenn@hotmail.com>
2023-04-29 17:17:31 +0100

    add test for spreadsheet.py

---
+++ src/spreadsheet_test.py
@@ -0,0 +1,32 @@
+"""Tests for spreadsheet.py."""
+import unittest
+from unittest.mock import patch, MagicMock
+from spreadsheet import Spreadsheet
+
+
+class TestSpreadsheet(unittest.TestCase):
+    @patch('spreadsheet.gspread.service_account')
+    def test_get_cols(self, mock_gspread_service_account):
+        mock_gspread_service_account.return_value = MagicMock()
+        # arrange
+        expected = [
+            ['Date', 'Spend', "Category"],
+            ['01/01/2021', '£10.00', 'Food'],
+            ['02/01/2021', '£20.00', 'Travel']
+        ]
+        spreadsheet = Spreadsheet("credentials_path", "spreadsheet_id")
+        # spreadsheet.spreadsheet.sheet1.col_values(i) returns a different column for each i
+        columns = [
+            ['Date', '01/01/2021', '02/01/2021'],
+            ['Spend', '£10.00', '£20.00'],
+            ['Category', 'Food', 'Travel']
+        ]
+        spreadsheet.spreadsheet.sheet1.col_values = MagicMock(
+            side_effect=lambda i: columns[i - 1]
+        )
+
+        # act
+        actual = spreadsheet.get_cols([1, 2, 3])
+
+        # assert
+        self.assertEqual(expected, actual)
