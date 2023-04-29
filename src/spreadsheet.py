bfa78d6bee53b11c00499a33e1feab6a74a75986
alifeee <alifeeerenn@hotmail.com>
2023-04-29 17:17:22 +0100

    Move dataframa out of spreadsheet class

--- src/spreadsheet.py
+++ src/spreadsheet.py
@@ -15,28 +15,38 @@ class Spreadsheet:
             filename=credentials_path)
         self.spreadsheet = self.gspread_credentials.open_by_key(spreadsheet_id)
 
-    def get_cols_as_df(self, column_numbers: list):
-        """Gets the specified columns as a dataframe.
+    def get_cols(self, column_numbers: list, ignore_first_row=False):
+        """Gets the specified columns as a 2d array.
 
         Args:
             column_numbers (list of ints): The column numbers to get. Starts at 1.
 
         Returns:
-            pandas.DataFrame: The columns as a dataframe.
+            list of lists: The columns as a 2d array.
         """
-        sheet = self.spreadsheet.sheet1
-        columns = [sheet.col_values(col) for col in column_numbers]
+        columns = [
+            self.spreadsheet.sheet1.col_values(col)
+            for col in column_numbers
+        ]
+        columns_2d = [list(x) for x in zip(*columns)]
 
-        dataframe = pandas.DataFrame(columns).transpose()
-
-        return dataframe
+        if ignore_first_row:
+            return columns_2d[1:]
+        return columns_2d
 
 
 if __name__ == '__main__':
+    # authentication
     CREDENTIALS_PATH = "google_credentials.json"
     SPREADSHEET_ID = "18OQs6uJgoyx3zrRhb9tcnBHxpUo4OyyFJKptJHMr-D0"
+    spreadsheet = Spreadsheet(CREDENTIALS_PATH, SPREADSHEET_ID)
 
-    ss = Spreadsheet(CREDENTIALS_PATH, SPREADSHEET_ID)
+    # data
+    cols = spreadsheet.get_cols([1, 2])
 
-    df = ss.get_cols_as_df([1, 2])
+    # dataframe conversion
+    df = pandas.DataFrame(cols[1:], columns=['Date', 'Spend'])
+    df['Date'] = pandas.to_datetime(df['Date'], format='%d/%m/%Y')
+    df['Spend'] = df['Spend'].str.strip('£').astype(float)
+
     print(df)
