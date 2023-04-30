d67055a583ba028a73570ddfeb497e9b750292e4
alifeee <alifeeerenn@hotmail.com>
2023-04-30 19:04:43 +0100

    open spreadsheet from userdata

--- budgeter/bothandlers/stats.py
+++ budgeter/bothandlers/stats.py
@@ -1,13 +1,36 @@
 from ..spreadsheet import SpreadsheetCredentials, Spreadsheet
+import telegram
 from telegram import Update
 from telegram.ext import ContextTypes, CommandHandler
 import pandas
+from gspread.exceptions import APIError
 
 
 async def stats(update: Update, context: ContextTypes.DEFAULT_TYPE, spreadsheet_credentials: SpreadsheetCredentials):
     message = await update.message.reply_text("Getting stats...")
-    SPREADSHEET_ID = "18OQs6uJgoyx3zrRhb9tcnBHxpUo4OyyFJKptJHMr-D0"
-    spreadsheet = Spreadsheet(spreadsheet_credentials, SPREADSHEET_ID)
+    try:
+        SPREADSHEET_ID = context.user_data["spreadsheet_id"]
+    except KeyError:
+        await message.edit_text(
+            "No spreadsheet ID found. Please create a spreadsheet first. /start",
+        )
+        return
+    
+    try:
+        spreadsheet = Spreadsheet(spreadsheet_credentials, SPREADSHEET_ID)
+    except APIError as e:
+        await message.edit_text(
+            f"""
+Error getting spreadsheet with ID:
+`{SPREADSHEET_ID}`
+            
+Check that the spreadsheet exists and that the bot has access to it\.
+
+Change the spreadsheet ID with /start
+""",
+            parse_mode="MarkdownV2"
+        )
+        return
     cols = spreadsheet.get_cols([1, 2])
     df = pandas.DataFrame(cols[1:], columns=['Date', 'Spend'])
     df['Date'] = pandas.to_datetime(df['Date'], format='%d/%m/%Y')
