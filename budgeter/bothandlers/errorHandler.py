991a37ac1bb6a97f18dba43072b749fb6cf36772
alifeee <alifeeerenn@hotmail.com>
2023-05-01 13:26:47 +0100

    fix error handler: errors do not always include user

--- budgeter/bothandlers/errorHandler.py
+++ budgeter/bothandlers/errorHandler.py
@@ -11,20 +11,29 @@ async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
         raise ValueError(
             "ADMIN_USER_ID environment variable not set."
         ) from e
+    try:
+        user = update.effective_user
+        chat = update.effective_chat
+        message = update.effective_message
+    except AttributeError:
+        user = "unknown"
+        chat = "unknown"
+        message = "unknown"
     await bot.send_message(
         chat_id=ADMIN_ID,
         text=f"""
 Error!
 
-User: {update.effective_user}
+User: {user}
 
-Chat: {update.effective_chat}
+Chat: {chat}
 
-Message: {update.effective_message}
+Message: {message}
 
 Error: {context.error}
         """
     )
+    print(context)
     logger = logging.getLogger(__name__)
     logger.error(
         f"Update {update} caused error {context.error}"
