302c8d7e5563f2ace23ce8127d60affd1c9d0863
alifeee <alifeeerenn@hotmail.com>
2023-05-01 12:41:49 +0100

    Make /start much clearer and better

--- budgeter/bothandlers/start.py
+++ budgeter/bothandlers/start.py
@@ -1,51 +1,106 @@
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram import InputMediaPhoto, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import ContextTypes, ConversationHandler, MessageHandler, CommandHandler, filters
 
 
-CHOOSING_SHEET_MODE, CONFIRMING, GIVING_SPREADSHEET_ID = range(3)
+USER_CHOOSING_SHEET_MODE, USER_CONFIRMING_CREATION, USERGIVING_SPREADSHEET_URL, USER_CONFIRMING_REMINDERS = range(4)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.effective_message.reply_text(
-        "Hi! I'm a bot that logs your daily spending. To get started, please create a spreadsheet.",
+    await update.effective_message.reply_html("""
+Hi!
+
+I help you log your daily spending over time. I can provide statistics as well as help keep the spreadsheet up to date.
+
+To get started let's create a spreadsheet where we can store your data. Use /cancel to cancel at any time.
+
+Created by @alifeeerenn. <a href="https://github.com/alifeee/telegram-budgeter">Source code</a>.
+""",
         reply_markup=ReplyKeyboardMarkup(
             [["Create spreadsheet"], ["Use existing spreadsheet"]], one_time_keyboard=True,
             is_persistent=True,
             resize_keyboard=True,
         ),
+        disable_web_page_preview=True,
     )
-    return CHOOSING_SHEET_MODE
+    return USER_CHOOSING_SHEET_MODE
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.effective_message.reply_text(
-        "Cancelled.",
+        "Cancelled. Use /start to start again.",
         reply_markup=ReplyKeyboardRemove(),
     )
     return ConversationHandler.END
 
 async def create_spreadsheet(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_photo(
+        "https://raw.githubusercontent.com/alifeee/telegram-budgeter/master/budgeter/bothandlers/images/new-google-sheet.png",
+        caption="""
+Let's get started! Create a new spreadsheet with <a href='https://sheets.google.com'>Google Sheets</a>.
+
+I only use the columns A and B, so make sure they have a label, and you can do what you want with the rest of the spreadsheet. Feel free to fill in as much historical data as you want! I will only ask for spending data from the earliest date that you have filled in.
+""",
+    parse_mode="HTML"
+    )
+    await update.message.reply_media_group(
+        [
+            InputMediaPhoto("https://raw.githubusercontent.com/alifeee/telegram-budgeter/master/budgeter/bothandlers/images/1-click-share.png"),
+            InputMediaPhoto("https://raw.githubusercontent.com/alifeee/telegram-budgeter/master/budgeter/bothandlers/images/2-enter-email.png"),
+            InputMediaPhoto("https://raw.githubusercontent.com/alifeee/telegram-budgeter/master/budgeter/bothandlers/images/3-share.png"),
+        ],
+        caption="""
+Now, to keep track of data I need access to the spreadsheet! Please share it by:
+
+1. Clicking the "Share" button in the top right corner
+2. Entering the email address of the bot 
+    <code>telegram-budgeter@telegram-budgeter.iam.gserviceaccount.com</code>
+3. Clicking "Share"
+        """
+    )
     await update.effective_message.reply_text(
-        "Please create a spreadsheet and confirm when you're done",
+        "Let me know when you're done!",
         reply_markup=ReplyKeyboardMarkup(
-            [["Confirm"]], one_time_keyboard=True, is_persistent=True, resize_keyboard=True,
+            [["I'm done"]], one_time_keyboard=True, is_persistent=True, resize_keyboard=True,
         ),
     )
-    return CONFIRMING
+    return USER_CONFIRMING_CREATION
 
 
 async def use_existing_spreadsheet(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.effective_message.reply_text(
-        "Please enter the spreadsheet URL",
+        "Please send me the URL of the spreadsheet you want to use. Make sure it is shared with the me so I can edit it!",
         reply_markup=ReplyKeyboardRemove(),
     )
-    return GIVING_SPREADSHEET_ID
+    return USERGIVING_SPREADSHEET_URL
 
 
 async def give_spreadsheet_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     spreadsheet_id = update.effective_message.text
     context.user_data["spreadsheet_url"] = spreadsheet_id
     await update.effective_message.reply_text(
-        "Spreadsheet URL received! You can now start logging your spending.",
+        "Thanks for the url! To get started, try /stats"
+    )
+    await update.effective_message.reply_text("Would you like me to ask you every morning how much you spent the day before, so you can keep track of spending? I'll also send some statistics each time.",
+        reply_markup=ReplyKeyboardMarkup(
+            [["Yes"], ["No"]], 
+            one_time_keyboard=True, 
+            is_persistent=True, 
+            resize_keyboard=True,
+        )
+    )
+    return USER_CONFIRMING_REMINDERS
+
+async def remind(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    context.user_data["reminders"] = True
+    await update.effective_message.reply_text(
+        "I'll remind you to log your spending every day at 10am. If you want to change this, use /remind",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    return ConversationHandler.END
+
+async def dont_remind(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    context.user_data["reminders"] = False
+    await update.effective_message.reply_text(
+        "Okay! If you change your mind, use /remind",
         reply_markup=ReplyKeyboardRemove(),
     )
     return ConversationHandler.END
@@ -53,7 +108,7 @@ async def give_spreadsheet_id(update: Update, context: ContextTypes.DEFAULT_TYPE
 start_handler = ConversationHandler(
     entry_points=[CommandHandler("start", start)],
     states={
-        CHOOSING_SHEET_MODE: [
+        USER_CHOOSING_SHEET_MODE: [
             MessageHandler(
                 filters.Regex("^Create spreadsheet$"), create_spreadsheet
             ),
@@ -62,15 +117,25 @@ start_handler = ConversationHandler(
                     "^Use existing spreadsheet$"), use_existing_spreadsheet
             ),
         ],
-        CONFIRMING: [
+        USER_CONFIRMING_CREATION: [
             MessageHandler(
-                filters.Regex("^Confirm$"),
+                filters.Regex("^I'm done$"),
                 use_existing_spreadsheet
             ),
         ],
-        GIVING_SPREADSHEET_ID: [
+        USERGIVING_SPREADSHEET_URL: [
             MessageHandler(
                 filters.TEXT, give_spreadsheet_id
+            ),
+        ],
+        USER_CONFIRMING_REMINDERS: [
+            MessageHandler(
+                filters.Regex("^Yes$"),
+                remind
+            ),
+            MessageHandler(
+                filters.Regex("^No$"),
+                dont_remind
             ),
         ],
     },
