a174f717621fbca35b87feffc60265171f547b52
alifeee <alifeeerenn@hotmail.com>
2023-05-07 00:15:54 +0100

    fix spend.py

--- budgeter/bothandlers/spend.py
+++ budgeter/bothandlers/spend.py
@@ -11,7 +11,6 @@ import datetime
 import pandas
 import numpy
 from .cancel import cancel_handler
-from gspread.client import Client
 
 USER_GIVING_DATA = range(1)
 
@@ -19,16 +18,27 @@ SPREADSHEET_NOT_SET_UP_MESSAGE = """
 You haven't set up a spreadsheet yet! Use /start to set one up.
 """
 
+SPREADSHEET_BADLY_FORMATTED_MESSAGE = """
+The spreadsheet appears to be broken. Try /start.
+"""
+
 UP_TO_DATE_MESSAGE = """
 You're up to date. Try /stats to see insights
 """
 
 SPENDING_DATA_MISSING_MESSAGE = """
-Spending data missing for {}}. How much did you spend on this day? ({}})
+Spending data missing for {}. How much did you spend on this day? ({})
 """
 
 NOT_A_NUMBER_MESSAGE = """
 That doesn't look like a number. Try again?
+"""
+
+DATA_NOT_ADDED_MESSAGE = """
+Data not added. Ask @alifeeerenn why. :)
+
+Or maybe this will help:
+(Error: {})
 """
 
 RECORDED_FINAL_SPEND_MESSAGE = """
@@ -41,23 +51,8 @@ Use /stats for more
 """
 
 RECORDED_INTERMEDIATE_SPEND_MESSAGE = """
-Recorded £{:.2f}! Spending data missing for {}}. How much did you spend on this day? ({})
+Recorded £{:.2f}! Spending data missing for {}. How much did you spend on this day? ({})
 """
-
-
-def get_first_day_without_data(dataframe: pandas.DataFrame) -> datetime:
-    """
-    Given a dataframe with "Date" and "Spend" columns, returns the first date where there is no spend data.
-
-    If there is no missing data, returns the date one day after the last date in the dataframe.
-    """
-    if len(dataframe) == 0:
-        return datetime.datetime.today() - datetime.timedelta(days=1)
-    first_empty_spend = dataframe[dataframe["Spend"].isna()]["Date"]
-    if len(first_empty_spend) > 0:
-        return first_empty_spend.min()
-    elif len(first_empty_spend) == 0:
-        return dataframe["Date"].max() + datetime.timedelta(days=1)
 
 
 async def spend(
@@ -71,21 +66,25 @@ async def spend(
         await message.edit_text(SPREADSHEET_NOT_SET_UP_MESSAGE)
         return ConversationHandler.END
 
-    context.user_data["spreadsheet_url"] = spreadsheet_url
     spreadsheet_client = context.bot_data["spreadsheet_client"]
     spreadsheet = Spreadsheet(spreadsheet_client, spreadsheet_url)
-    df = spreadsheet.get_parsed_data()
+    try:
+        df = spreadsheet.get_spending_dataframe()
+    except Exception as e:
+        await message.edit_text(SPREADSHEET_BADLY_FORMATTED_MESSAGE)
+        return ConversationHandler.END
 
-    first_no_data = get_first_day_without_data(df)
+    next_date = df["Date"].max() + datetime.timedelta(days=1)
     today = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-    if first_no_data >= today:
+
+    if next_date >= today:
         await message.edit_text(UP_TO_DATE_MESSAGE)
         return ConversationHandler.END
 
-    context.user_data["date"] = first_no_data
-    date = first_no_data.strftime("%d/%m/%Y")
-    dayofweek = first_no_data.strftime("%A")
-    daysago = (datetime.datetime.now() - first_no_data).days
+    context.user_data["date"] = next_date
+    date = next_date.strftime("%d/%m/%Y")
+    dayofweek = next_date.strftime("%A")
+    daysago = (datetime.datetime.now() - next_date).days
     if daysago == 1:
         daysagotext = "yesterday"
     else:
@@ -111,10 +110,16 @@ async def give_data(
     spreadsheet_url = context.user_data["spreadsheet_url"]
     spreadsheet_client = context.bot_data["spreadsheet_client"]
     spreadsheet = Spreadsheet(spreadsheet_client, spreadsheet_url)
-    df = spreadsheet.add_data(context.user_data["date"], amount)
-    first_no_data = get_first_day_without_data(df)
+    data_added, why_not = spreadsheet.add_data(context.user_data["date"], amount)
+    if not data_added:
+        await message.edit_text(DATA_NOT_ADDED_MESSAGE.format(why_not))
+        return ConversationHandler.END
+
+    df = spreadsheet.get_spending_dataframe()
+
+    next_date = df["Date"].max() + datetime.timedelta(days=1)
     today = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-    if first_no_data >= today:
+    if next_date >= today:
         avg30d = df["Spend"].tail(30).mean()
         avgdiff = df["Spend"].tail(31).head(30).mean() - avg30d
         avgarrow = "⬆️" if avgdiff > 0 else "⬇️" if avgdiff < 0 else "➡️"
@@ -122,11 +127,11 @@ async def give_data(
             RECORDED_FINAL_SPEND_MESSAGE.format(amount, avg30d, avgarrow, abs(avgdiff))
         )
         return ConversationHandler.END
-    elif first_no_data < datetime.datetime.now():
-        context.user_data["date"] = first_no_data
-        date = first_no_data.strftime("%d/%m/%Y")
-        dayofweek = first_no_data.strftime("%A")
-        daysago = (datetime.datetime.now() - first_no_data).days
+    elif next_date < datetime.datetime.now():
+        context.user_data["date"] = next_date
+        date = next_date.strftime("%d/%m/%Y")
+        dayofweek = next_date.strftime("%A")
+        daysago = (datetime.datetime.now() - next_date).days
         if daysago == 1:
             daysagotext = "yesterday"
         else:
