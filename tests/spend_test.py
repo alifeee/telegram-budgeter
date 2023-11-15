51747b2626e25d88c218e16bc309200c41d4b6d6
alifeee <alifeeerenn@hotmail.com>
2023-11-15 11:21:32 +0000

    formatting

--- tests/spend_test.py
+++ tests/spend_test.py
@@ -5,16 +5,20 @@ import sys
 import pandas
 import datetime
 import numpy
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from budgeter.bothandlers.spend import get_first_day_without_data
+
 
 class TestGetFirstDayWithoutData(unittest.TestCase):
     def test_simple_df(self):
         # Arrange
-        df = pandas.DataFrame({
-            "Date": ["01/01/2021", "02/01/2021", "03/01/2021"],
-            "Spend": [20, 15, numpy.nan]
-        })
+        df = pandas.DataFrame(
+            {
+                "Date": ["01/01/2021", "02/01/2021", "03/01/2021"],
+                "Spend": [20, 15, numpy.nan],
+            }
+        )
         df["Date"] = pandas.to_datetime(df["Date"], format="%d/%m/%Y")
         expected = datetime.datetime(2021, 1, 3)
 
@@ -26,10 +30,9 @@ class TestGetFirstDayWithoutData(unittest.TestCase):
 
     def test_no_missing_data(self):
         # Arrange
-        df = pandas.DataFrame({
-            "Date": ["01/01/2021", "02/01/2021", "03/01/2021"],
-            "Spend": [20, 15, 10]
-        })
+        df = pandas.DataFrame(
+            {"Date": ["01/01/2021", "02/01/2021", "03/01/2021"], "Spend": [20, 15, 10]}
+        )
         df["Date"] = pandas.to_datetime(df["Date"], format="%d/%m/%Y")
         expected = datetime.datetime(2021, 1, 4)
 
@@ -42,10 +45,7 @@ class TestGetFirstDayWithoutData(unittest.TestCase):
     @freeze_time("2021-01-05")
     def test_empty_df(self):
         # Arrange
-        df = pandas.DataFrame({
-            "Date": [],
-            "Spend": []
-        })
+        df = pandas.DataFrame({"Date": [], "Spend": []})
         expected = datetime.datetime(2021, 1, 4)
 
         # Act
